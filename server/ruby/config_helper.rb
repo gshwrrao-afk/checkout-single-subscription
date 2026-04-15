2e38988fc9ccc035cb64d2c8eb042eebbfe5780b
Catherine Moresco <cmoresco@stripe.com>
2026-04-15 11:42:11 -0400

    Add reminders about security best practices and redact secrets in logs

--- server/ruby/config_helper.rb
+++ server/ruby/config_helper.rb
@@ -123,6 +123,14 @@ class ConfigHelper
     true
   end
 
+  def redact_key(key)
+    return '(unset)' if key.nil? || key == ''
+
+    return key[0, 4] + ('*' * (key.length - 4)) if key.length <= 14
+
+    "#{key[0, 8]}#{'*' * (key.length - 14)}#{key[-6..]}"
+  end
+
   def valid_paths?
     static_dir = ENV['STATIC_DIR']
     if static_dir.nil?
@@ -190,7 +198,7 @@ class ConfigHelper
         api_key: sk
       }).data.first
     rescue => e
-      puts "Failed testing an API request with your STRIPE_SECRET_KEY `#{sk}` check `.env`: \n\n#{e}"
+      puts "Failed testing an API request with your STRIPE_SECRET_KEY `#{redact_key(sk)}` check `.env`: \n\n#{e}"
       return false
     end
 
